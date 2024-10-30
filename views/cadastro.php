358178fcb6a6e83552a0d2a5417abbbf45b93c31
andrehelio <andrelhc1020@gmail.com>
2024-10-30 12:24:21 -0300

    update cadastro html e css

--- views/cadastro.php
+++ views/cadastro.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../public/css/cadastro.css">
+    <link rel="stylesheet" href="../css/cadastro.css">
     <title>Cadastro</title>
 </head>
 
@@ -17,46 +17,41 @@
             <div class="form-input">
                 <label for="nome">Nome</label>
                 <div class="circulo"></div>
-                <img class="img-input" src="../public/imgs/user_img.svg" alt="">
-                <input type="text" class="nome" id="nome" name="nome" placeholder="Ex.: Rômulo Ferreira Santos">
+                <img class="img-input" src="../img/user_img.svg" alt="">
+                <input type="text" class="nome" id="nome" placeholder="Ex.: Rômulo Ferreira Santos">
             </div>
             <div class="form-input">
                 <label for="cpf">CPF</label>
                 <div class="circulo"></div>
-                <img class="img-input" src="../public/imgs/cpf_img.svg" alt="">
-                <input type="text" class="cpf" id="cpf" name="cpf" pattern="\d{3}(\.?\d{3}){2}-?\d{2}|^\d{11}$"
+                <img class="img-input" src="../img/cpf_img.svg" alt="">
+                <input type="text" class="cpf" id="cpf" pattern="\d{3}(\.?\d{3}){2}-?\d{2}|^\d{11}$"
                     placeholder="Ex.: 000.000.000-00">
             </div>
             <div class="form-input">
                 <label for="data-nasc">Data de nascimento</label>
                 <div class="circulo"></div>
-                <img class="img-input" src="../public/imgs/calendario_img.svg" alt="">
-                <input type="text" class="data-nasc" id="data" name="data-nasc" placeholder="Ex.: 01/01/1999">
+                <img class="img-input" src="../img/calendario_img.svg" alt="">
+                <input type="text" class="data-nasc" id="data" placeholder="Ex.: 01/01/1999">
             </div>
             <div class="form-input">
                 <label for="email">E-mail</label>
                 <div class="circulo"></div>
-                <img class="img-input" src="../public/imgs/envelope_img.svg" alt="">
-                <input type="email" class="email" id="email" name="email" placeholder="Ex.: senac321@petrolina.pe.senac.br">
+                <img class="img-input" src="../img/envelope_img.svg" alt="">
+                <input type="email" class="email" id="email" placeholder="Ex.: senac321@petrolina.pe.senac.br">
             </div>
             <div class="form-input">
                 <label for="senha">Senha</label>
                 <div class="circulo"></div>
-                <img class="img-input" src="../public/imgs/cadeado_img.svg" alt="">
-                <input type="password" class="senha" id="senha" name="senha" placeholder="Ex.: senac123">
-            </div>
-            <div class="erro-php">
-                <?php
-                
-                ?>
+                <img class="img-input" src="../img/cadeado_img.svg" alt="">
+                <input type="password" class="senha" id="senha" placeholder="Ex.: senac123">
             </div>
             <button class="botao-cadastro">CRIAR CONTA</button>
-            <p class="text">Já possui conta? <a href="login.html">Log-in</a></p>
+            <p class="text">Já possui conta? <a href="Login.html">Log-in</a></p>
         </div>
     </main>
     <div class="triangulo-principal"></div>
     <div class="senac">
-        <img src="../public/imgs/logo_branco 1.png" alt="Logo do Senac">
+        <img src="../img/logo_branco 1.png" alt="Logo do Senac">
     </div>
     <div class="texto-triangulo">
         <h2>Sistema de Gerenciamento</h2>
